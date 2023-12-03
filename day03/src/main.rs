9aab406efa1a501ae5473ea9f45564eb1d6426db
jiricodes <novotny.049@gmail.com>
2023-12-03 20:19:06 +0200

    day03 part1 kinda silly

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -17,19 +17,21 @@ impl Pos {
 
     fn neighbours(&self, length: u32) -> Vec<Pos> {
         let mut ngbs: Vec<Pos> = Vec::new();
+        let mut start_t: u32 = self.x;
         // left
         if self.x > 0 {
             ngbs.push(Pos::new(self.x - 1, self.y));
+            start_t -= 1;
         }
         // right
         ngbs.push(Pos::new(self.x + length, self.y)); 
-        for t in self.x..(self.x + length) {
+        for t in start_t..(self.x + length + 1) {
             // top
             if self.y > 0 {
-                ngbs.push(Pos::new(self.x + t, self.y - 1)); 
+                ngbs.push(Pos::new(t, self.y - 1)); 
             }
             // bot
-            ngbs.push(Pos::new(self.x + t, self.y + 1)); 
+            ngbs.push(Pos::new(t, self.y + 1)); 
         }
 
         ngbs
@@ -67,6 +69,13 @@ impl Component {
     pub fn constrain_area(&mut self, area: Area) {
         self.neigbours_pos.retain(|&x| area.is_within(x));
     }
+
+    pub fn new(value: u32, neigbours_pos: Vec<Pos>) -> Self {
+        Self {
+            value,
+            neigbours_pos,
+        }
+    }
 }
 
 
@@ -86,9 +95,51 @@ impl Schematic {
 
     pub fn parse_line(&mut self, line: &str) {
         self.area.end.x = line.len() as u32;
+        let y: u32 = self.area.end.y;
         self.area.end.y += 1;
-        let chunks: Vec<&str> = line.split(".").collect();
-        dbg!(chunks);
+
+
+        let bytes = line.as_bytes();
+        let mut i = 0;
+        while i < bytes.len() {
+            if bytes[i] == 46 {
+                i += 1;
+            } else if !bytes[i].is_ascii_digit() {
+                self.symbols.push(Pos::new(i as u32, y));
+                i += 1;
+            } else {
+                let x = i as u32;
+                while i < bytes.len() && bytes[i].is_ascii_digit() {
+                    i += 1;
+                }
+                let val: u32 = line[x as usize..i].parse().unwrap();
+                let pos = Pos::new(x, y);
+                let new_component = Component::new(val, pos.neighbours(i as u32 - x));
+                self.components.push(new_component);
+
+            }
+        }
+        return;
+        
+
+        // let chunks: Vec<&str> = line.split(".").collect();
+        // for chunk in chunks {
+        //     if chunk == "" {
+        //         x += 1;
+        //         dbg!(x, "empty");
+        //         continue;
+        //     } else if chunk.as_bytes().iter().all(|x| x.is_ascii_digit()) {
+        //         let pos = Pos::new(x, y);
+        //         dbg!(pos);
+        //         let len = chunk.len() as u32;
+        //         dbg!(len);
+        //         let val: u32 = chunk.parse().unwrap();
+        //         let new_component = Component::new(val, pos.neighbours(len));
+        //         x += len;
+        //         dbg!(x);
+        //         self.components.push(new_component);
+        //     }
+        // }
     }
 
     pub fn new() -> Self {
@@ -121,12 +172,11 @@ fn part_one(filename: &str) {
 
     let mut schematic = Schematic::new();
     for line in lines {
-
         if let Ok(text) = line {
             schematic.parse_line(&text);
         }
     }
-    dbg!(&schematic);
+    // dbg!(&schematic);
 
     println!("Part 1: {}", schematic.calc_part_one());
 }
