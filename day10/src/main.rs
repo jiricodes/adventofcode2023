18ac9a3de202c69960578847ba5b041fd6fa70ea
jiricodes <novotny.049@gmail.com>
2023-12-10 20:41:17 +0200

    day10 part2 inside Display trait lol

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fmt::Display,
     fs::File,
     io::{BufRead, BufReader},
     ops::{Add, Mul},
@@ -36,7 +37,7 @@ impl Mul<i32> for Point {
     }
 }
 
-#[derive(Debug, Clone, Copy, Default)]
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
 struct Pipe(Point, Point);
 
 impl Pipe {
@@ -46,6 +47,14 @@ impl Pipe {
     const BEND_J: Self = Self(Point::NORTH, Point::WEST);
     const BEND_7: Self = Self(Point::SOUTH, Point::WEST);
     const BEND_F: Self = Self(Point::SOUTH, Point::EAST);
+    const ALL: [Self; 6] = [
+        Self::VERT,
+        Self::HORZ,
+        Self::BEND_L,
+        Self::BEND_J,
+        Self::BEND_7,
+        Self::BEND_F,
+    ];
 
     fn conn_north(&self) -> bool {
         self.0 == Point::NORTH || self.1 == Point::NORTH
@@ -109,6 +118,21 @@ impl From<char> for Cell {
     }
 }
 
+impl Display for Cell {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let repr = match self.kind {
+            Some(Pipe::HORZ) => '-',
+            Some(Pipe::VERT) => '|',
+            Some(Pipe::BEND_L) => '╰',
+            Some(Pipe::BEND_J) => '╯',
+            Some(Pipe::BEND_7) => '╮',
+            Some(Pipe::BEND_F) => '╭',
+            _ => ' ',
+        };
+        write!(f, "{}", repr)
+    }
+}
+
 #[derive(Debug, Clone, Default)]
 struct Map {
     grid: Vec<Cell>,
@@ -152,6 +176,31 @@ impl Map {
         len
     }
 
+    fn pipe_path(&self) -> Vec<Point> {
+        let mut path: Vec<Point> = vec![self.start];
+        let mut current: Point = Point::default();
+        let mut from_dir: Point = Point::default();
+        // find first pipe the stupid way
+        for dir in Point::ALL_DIRS {
+            current = *path.last().unwrap() + dir;
+            if let Some(cell) = self.get(current) {
+                if cell.has_conn_to_opposite(dir) {
+                    from_dir = dir * -1;
+                    break;
+                }
+            }
+        }
+        while current != self.start {
+            path.push(current);
+            let cell = self.get(current).expect("failed to get connecting cell");
+            let pipe = cell.kind.expect("failed to get connecting pipe");
+            let new_dir = pipe.get_other(from_dir);
+            current = current + new_dir;
+            from_dir = new_dir * -1;
+        }
+        path
+    }
+
     fn get(&self, pos: Point) -> Option<&Cell> {
         let i = self.dimension.x * pos.y + pos.x;
         if i < 0 {
@@ -159,6 +208,82 @@ impl Map {
         } else {
             self.grid.get(i as usize)
         }
+    }
+
+    fn add_start_pipe(&mut self) {
+        let mut flag = false;
+        let mut a = Pipe::default();
+        let mut b = Pipe::default();
+        for dir in Point::ALL_DIRS {
+            let current = self.start + dir;
+            if let Some(cell) = self.get(current) {
+                if cell.has_conn_to_opposite(dir) {
+                    if !flag {
+                        flag = true;
+                        a.0 = dir;
+                        b.1 = dir;
+                    } else {
+                        a.1 = dir;
+                        b.0 = dir;
+                    }
+                }
+            }
+        }
+        let i = (self.dimension.x * self.start.y + self.start.x) as usize;
+        if Pipe::ALL.contains(&a) {
+            self.grid[i].kind = Some(a);
+        } else {
+            self.grid[i].kind = Some(b);
+        }
+    }
+
+    fn clear_map(&mut self, path: Vec<Point>) {
+        let indeces: Vec<usize> = path
+            .iter()
+            .map(|p| (self.dimension.x * p.y + p.x) as usize)
+            .collect();
+        for (i, cell) in self.grid.iter_mut().enumerate() {
+            if indeces.contains(&i) {
+                continue;
+            } else {
+                cell.kind = None;
+            }
+        }
+    }
+}
+
+impl Display for Map {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut repr: String = String::new();
+        // maybe I should not be doing part2 solution in display trait xD
+        let mut part2_count = 0;
+        for y in 0..self.dimension.y {
+            let mut inside = false;
+            for x in 0..self.dimension.x {
+                // if self.start.x == x && self.start.y == y {
+                //    repr.push('S');
+                // } else {
+                let cell = self.get(Point::new(x, y)).unwrap();
+                let cell_repr = format!("{}", cell);
+                match cell_repr.as_str() {
+                    " " => {
+                        if inside {
+                            repr += "░";
+                            part2_count += 1;
+                        } else {
+                            repr += &cell_repr;
+                        }
+                    }
+                    "|" | "╮" | "╭" =>  { inside = !inside; repr += &cell_repr;}
+                    _ => {repr += &cell_repr;}
+                }
+                // }
+            }
+            if y != self.dimension.y - 1 {
+                repr += "\n";
+            }
+        }
+        write!(f, "{}\nPart 2: {}", repr, part2_count)
     }
 }
 
@@ -183,11 +308,21 @@ fn part_two(filename: &str) {
     let file = File::open(filename).expect("Failed to open file");
     let lines = BufReader::new(&file).lines();
 
-    let mut total = 0;
+    let mut map: Map = Map::default();
+    let mut y: i32 = 0;
     for line in lines {
-        if let Ok(text) = line {}
+        if let Ok(text) = line {
+            for (x, c) in text.chars().enumerate() {
+                map.add_cell(c, Point::new(x as i32, y));
+            }
+            y += 1;
+        }
     }
-    println!("Part 2: {}", total);
+    let path = map.pipe_path();
+    map.clear_map(path);
+    map.add_start_pipe();
+    println!("{}", map);
+    //println!("Part 2: {}", map.pipe_len() / 2);
 }
 
 fn main() {
